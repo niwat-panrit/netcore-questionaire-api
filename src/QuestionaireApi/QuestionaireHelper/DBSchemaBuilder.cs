a1f2ded05aeaa4d0a189d0094d53f4db65a62ce3
niwat-panrit <niwat.panrit@gmail.com>
2021-11-02 21:39:29 +0700

    Added DB schema builder

---
+++ src/QuestionaireApi/QuestionaireHelper/DBSchemaBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NHibernate.Tool.hbm2ddl;
+using Questionaire.common.datastore;
+using static QuestionaireHelper.Program;
+
+namespace QuestionaireHelper
+{
+    class DBSchemaBuilder : ActionHandlerBase
+    {
+        public DBSchemaBuilder()
+        {
+            Logger?.Info($"Starting {nameof(DBSchemaBuilder)}...");
+        }
+
+        public override void Run(Options o)
+        {
+            Logger?.Info($"Exporting DB schema...");
+
+            var schemaExport = QuestionnaireDataStore.Instance
+                .GetSchemaExport();
+            var ddlFile = "hbm2schema.sql";
+            schemaExport.SetOutputFile(ddlFile);
+            schemaExport.Create(true, true);
+
+            Logger?.Info($"DB schema exported to {ddlFile}.");
+        }
+    }
+}
--- src/QuestionaireApi/QuestionaireHelper/Program.cs
+++ src/QuestionaireApi/QuestionaireHelper/Program.cs
@@ -13,6 +13,9 @@ namespace QuestionaireHelper
         {
             [Option("load-countries", Required = false, HelpText = "Load country list as a choice group.")]
             public bool LoadCountries { get; set; }
+
+            [Option("export-db-schema", Required = false, HelpText = "Export DB schema.")]
+            public bool ExportDBSchema { get; set; }
         }
 
         static void Main(string[] args)
@@ -30,6 +33,9 @@ namespace QuestionaireHelper
                    {
                        if (o.LoadCountries)
                            (new CountryListBuilder()).Run(o);
+
+                       if (o.ExportDBSchema)
+                           (new DBSchemaBuilder()).Run(o);
                    })
                    .WithNotParsed<Options>(o =>
                    {
