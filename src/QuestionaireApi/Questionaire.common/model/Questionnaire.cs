9c1450ed9f7830c804475f09c5605d71a53ccd74
Niwat Panrit <niwat.panrit@gmail.com>
2021-12-24 20:38:24 +0700

    Update Questionnaire.cs

--- src/QuestionaireApi/Questionaire.common/model/Questionnaire.cs
+++ src/QuestionaireApi/Questionaire.common/model/Questionnaire.cs
@@ -22,39 +22,5 @@ namespace Questionaire.common.model
         public Questionnaire()
         {
         }
-
-        public virtual IEnumerable<Question> GetQuestions()
-        {
-            return QuestionDataStore.Instance
-                .GetQuestions(this.ID);
-        }
-
-        public virtual IEnumerable<IEnumerable<Answer>> GetAnswers()
-        {
-            var answers = AnswerDataStore.Instance
-                .GetAnswers(this.ID);
-
-            // TODO:
-            //  1) Use SQL join
-            //  2) Order answers by question's display order
-
-            return answers.GroupBy(a => a.SessionID);
-        }
-
-        public virtual Question GetPreviousQuestion(Question question) =>
-            QuestionaireDataStore.Instance
-                .GetPreviousQuestion(question);
-
-        public virtual Question GetNextQuestion(Question question) =>
-            QuestionaireDataStore.Instance
-                .GetNextQuestion(question);
-
-        public virtual int GetMinDisplayOrder() =>
-            QuestionaireDataStore.Instance
-                .GetMinDisplayOrder(this.ID);
-
-        public virtual int GetMaxDisplayOrder() =>
-            QuestionaireDataStore.Instance
-                .GetMaxDisplayOrder(this.ID);
     }
 }
