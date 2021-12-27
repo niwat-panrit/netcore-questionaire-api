bdedaca15257e4e52a03e851be47a64ea85496e5
Niwat Panrit <niwat.panrit@gmail.com>
2021-12-27 21:42:27 +0700

    Reimplement AnswerDataStore

---
+++ src/QuestionaireApi/Questionaire.common/data/QuestionaireResultSet.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Questionaire.common.model;
+
+namespace Questionaire.common.data
+{
+    public class QuestionaireResultSet
+	{
+        private readonly Dictionary<int, Question> questions =
+            new Dictionary<int, Question>();
+
+        /// <summary>
+        /// Rows and Columns of answers:
+        ///     Top row is header which is list of question's title.
+        ///     Each row (Row ID = Session ID) represent a set of answer from the Questionnaire's questions.
+        ///     Each column (Column ID = Question ID) represent answer(s) of each question.
+        /// </summary>
+        public DataTable AnswerSheet { get; } =
+            new DataTable();
+
+        /// <summary>
+        /// Instantiate new object of <see cref="QuestionaireResultSet"/>
+        /// </summary>
+        /// <param name="questions"><see cref="QuestionaireResultSet"/>'s <see cref="Question"/>s</param>
+        public QuestionaireResultSet(IList<Question> questions)
+        {
+            var headerRow = this.AnswerSheet.NewRow();
+
+            foreach (var question in questions)
+            {
+                if (this.questions.ContainsKey(question.ID))
+                    throw new InvalidOperationException($"Duplicated question(s)!");
+
+                this.questions.Add(question.ID, question);
+
+                var columnID = question.ID.ToString();
+                this.AnswerSheet.Columns
+                    .Add(columnID, typeof(AnswerSet));
+                headerRow[columnID] = question.Label;
+            }
+
+            this.AnswerSheet.Rows.Add(headerRow);
+        }
+
+        /// <summary>
+        /// Read specified <see cref="Answer"/>s into the <see cref="AnswerSheet"/>
+        /// </summary>
+        /// <param name="answers">List of <see cref="Answer"/></param>
+        /// <exception cref="InvalidOperationException">When <see cref="Answer"/> doesn't belong to any <see cref="Question"/> in the <see cref="Questionaire"/></exception>
+        public void Read(IList<Answer> answers)
+        {
+            /// Session ID => associated <see cref="DataRow"/>
+            var dataRowIndex = new Dictionary<int, DataRow>();
+
+            foreach (var record in answers)
+            {
+                /// Get reference to the source question and also validate relation between <see cref="Answer"/> and <see cref="Question"/>
+                if (!this.questions.TryGetValue(record.QuestionID, out Question sourceQuestion))
+                    throw new InvalidOperationException($"The answer doesn't belong to any question in the questionnaire!");
+
+                if (!dataRowIndex.TryGetValue(record.SessionID, out DataRow dataRow))
+                    dataRowIndex.Add(record.SessionID, dataRow = this.AnswerSheet.NewRow()); // Add new data row into the sheet
+
+                var columndID = record.QuestionID.ToString();
+                var answerSet = (AnswerSet)dataRow[columndID];
+                if (answerSet != null)
+                    dataRow[columndID] = // Add new column into the row
+                        answerSet = new AnswerSet(sourceQuestion);
+                answerSet.Answers.Add(record); // Add value into the column
+            }
+        }
+
+        public class AnswerSet
+        {
+            public Question Question { get; }
+
+            public List<Answer> Answers { get; } =
+                new List<Answer>();
+
+            public AnswerSet(Question sourceQuestion)
+            {
+                this.Question = sourceQuestion;
+            }
+        }
+    }
+}
--- src/QuestionaireApi/Questionaire.common/datastore/AnswerDataStore.cs
+++ src/QuestionaireApi/Questionaire.common/datastore/AnswerDataStore.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Extensions.Caching.Memory;
 using NHibernate;
+using Questionaire.common.data;
 using Questionaire.common.model;
 using Questionaire.common.tool;
 
@@ -9,51 +9,84 @@ namespace Questionaire.common.datastore
 {
     public class AnswerDataStore : DataStoreBase
     {
-        // TODO: Optimize parameters
+        private readonly ICache<ISession> sessionCache;
+        private readonly IMemoryCache dataCache;
+        private readonly AnswerExceptionDataStore answerExceptionDataStore;
 
-        private ICache<ISession> sessionCache;
-
-        public AnswerDataStore(DataStoreConfig config)
+        public AnswerDataStore(
+            DataStoreConfig config,
+            ICache<ISession> sessionCache,
+            IMemoryCache dataCache,
+            AnswerExceptionDataStore answerExceptionDataStore)
             : base(config)
         {
-            this.sessionCache = new DataStoreSessionCache(this);
+            this.sessionCache = sessionCache;
+            this.dataCache = dataCache;
+            this.answerExceptionDataStore = answerExceptionDataStore;
         }
 
-        public IEnumerable<IEnumerable<Answer>> GetAnswers(Questionnaire questionaire)
+        /// <summary>
+        /// Get all submitted answers of specified <see cref="Questionnaire"/>.
+        /// </summary>
+        /// <param name="questionaire">Querying <see cref="Questionnaire"/>.</param>
+        /// <returns>
+        /// Answers of <see cref="Questionnaire"/> group by question.
+        /// (Some <see cref="Question"/> may has multiple answers.)
+        /// </returns>
+        public QuestionaireResultSet GetAnswers(Questionnaire questionaire)
         {
-            // TODO:
-            //  1) Use SQL join
-            //  2) Order answers by question's display order
+            var cacheKey = questionaire;
 
+            // Try to get result from the cache for faster result
+            if (this.dataCache.TryGetValue(cacheKey, out QuestionaireResultSet cachedResult))
+                return cachedResult;
+
+            // Cache isn't available, build new result
             using (var dbSession = OpenStatelessSession())
-                return dbSession.QueryOver<Answer>()
+            using (var transaction = dbSession.BeginTransaction(System.Data.IsolationLevel.ReadCommitted))
+            {
+                var questions = dbSession.QueryOver<Question>()
+                    .Where(q => q.QuestionnaireID == questionaire.ID)
+                    .OrderBy(q => q.DisplayOrder).Asc
+                    .ThenBy(q => q.CreatedAt).Asc
+                    .List();
+                var result = new QuestionaireResultSet(questions);
+                var answers = dbSession.QueryOver<Answer>()
                     .Where(a => a.QuestionnaireID == questionaire.ID)
-                    .List().GroupBy(a => a.SessionID);
+                    .OrderBy(a => a.CreatedAt).Asc
+                    .List();
+                result.Read(answers);
+
+                // Cache result in the memory for 30 seconds
+                // TODO: Configurable cache period
+                this.dataCache.Set(cacheKey, result, TimeSpan.FromSeconds(30));
+
+                return result;
+            }
         }
 
-        public bool Submit(Session session, Question question, Answer newAnswer, bool more, List<string> warnings, out bool isException)
+        public bool Submit(Session qSession, Question question, Answer answer, bool more, out bool isException)
         {
-            // TODO: Check relation of questionnaire, session and quest ids in answer
+            if (!qSession.QuestionnaireID.Equals(question.QuestionnaireID) ||
+                !question.ID.Equals(answer.QuestionID))
+                throw new InvalidOperationException($"Invalid submission!");
 
             var cacheKey = question;
 
             try
             {
-                var dbSession = this.sessionCache.GetSession(cacheKey);
+                var dbSession = this.sessionCache.Get(cacheKey);
 
-                newAnswer.CreatedAt =
-                    newAnswer.UpdatedAt =
-                        DateTime.Now;
-                dbSession.Save(newAnswer);
-                isException = AnswerExceptionDataStore.Instance
-                    .IsExceptionalAnswer(question, newAnswer, dbSession);
+                var answerID = dbSession.Save(answer);
+                isException = this.answerExceptionDataStore
+                    .IsExceptionalAnswer(question, answer, dbSession);
 
-                return true;
+                return answerID != null;
             }
             finally
             {
                 if (!more)
-                    this.sessionCache.ClearSession(cacheKey);
+                    this.sessionCache.Clear(cacheKey);
             }
         }
     }
