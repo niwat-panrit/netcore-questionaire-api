e9b26e17063870c42014d69030d2c871eba18045
Niwat Panrit <niwat.panrit@gmail.com>
2022-01-10 22:02:24 +0700

    Optimize ChoiceGroupDataStore

--- src/QuestionaireApi/Questionaire.common/datastore/ChoiceGroupDataStore.cs
+++ src/QuestionaireApi/Questionaire.common/datastore/ChoiceGroupDataStore.cs
@@ -5,39 +5,51 @@ namespace Questionaire.common.datastore
 {
     public class ChoiceGroupDataStore : DataStoreBase
     {
-        // TODO: Optimize parameters
-
         public ChoiceGroupDataStore(DataStoreConfig config)
             : base(config)
         {
         }
 
+        /// <summary>
+        /// Get specific choice group from ID.
+        /// </summary>
+        /// <param name="groupID">The choice group identifier.</param>
+        /// <returns>Choice group</returns>
         public ChoiceGroup GetGroup(int groupID)
         {
-            using (var dbSession = OpenStatelessSession())
+            using (var dbSession = OpenSession())
                 return dbSession.QueryOver<ChoiceGroup>()
                     .Where(c => c.ID == groupID)
                     .Take(1).SingleOrDefault();
         }
 
+        /// <summary>
+        /// Get specific choice group from name.
+        /// </summary>
+        /// <param name="groupName">Group name</param>
+        /// <returns>Choice group</returns>
         public ChoiceGroup GetByName(string groupName)
         {
-            using (var dbSession = OpenStatelessSession())
+            using (var dbSession = OpenSession())
                 return dbSession.QueryOver<ChoiceGroup>()
                     .Where(c => c.Name == groupName)
                     .Take(1).SingleOrDefault();
         }
 
-        public ChoiceGroup Create(ChoiceGroup choiceGroup)
+        /// <summary>
+        /// Save new choice group.
+        /// </summary>
+        /// <param name="choiceGroup">The new choice group</param>
+        public void Create(ChoiceGroup choiceGroup)
         {
-            using (var dbSession = OpenStatelessSession())
+            using (var dbSession = OpenSession())
             {
+                // TODO: Use DB's on create and update
                 choiceGroup.CreatedAt =
                     choiceGroup.UpdatedAt =
                         DateTime.Now;
-                choiceGroup.ID = (int)dbSession.Insert(choiceGroup);
 
-                return choiceGroup;
+                dbSession.Save(choiceGroup);
             }
         }
     }
