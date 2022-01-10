73c7ef6430780afbb896ff58fc4ef5891002c28e
Niwat Panrit <niwat.panrit@gmail.com>
2022-01-10 21:36:38 +0700

    Optimize AnswerDataStore

--- src/QuestionaireApi/Questionaire.common/datastore/AnswerDataStore.cs
+++ src/QuestionaireApi/Questionaire.common/datastore/AnswerDataStore.cs
@@ -3,24 +3,20 @@ using Microsoft.Extensions.Caching.Memory;
 using NHibernate;
 using Questionaire.common.data;
 using Questionaire.common.model;
-using Questionaire.common.tool;
 
 namespace Questionaire.common.datastore
 {
     public class AnswerDataStore : DataStoreBase
     {
-        private readonly ICache<ISession> sessionCache;
         private readonly IMemoryCache dataCache;
         private readonly AnswerExceptionDataStore answerExceptionDataStore;
 
         public AnswerDataStore(
             DataStoreConfig config,
-            ICache<ISession> sessionCache,
             IMemoryCache dataCache,
             AnswerExceptionDataStore answerExceptionDataStore)
             : base(config)
         {
-            this.sessionCache = sessionCache;
             this.dataCache = dataCache;
             this.answerExceptionDataStore = answerExceptionDataStore;
         }
@@ -65,29 +61,49 @@ namespace Questionaire.common.datastore
             }
         }
 
-        public bool Submit(Session qSession, Question question, Answer answer, bool more, out bool isException)
+        /// <summary>
+        /// Submit an answer and check whether the answer is exceptional answer on specified question.
+        /// </summary>
+        /// <param name="qSession">The session submitted from.</param>
+        /// <param name="question">The question of submitting answer.</param>
+        /// <param name="answer">The answer.</param>
+        /// <param name="isException">Indicate whether the specified answer is exceptional answer.</param>
+        /// <returns>True on success, False otherwise.</returns>
+        public bool Submit(Session qSession, Question question, Answer answer, out bool isException)
         {
+            using (var dbSession = OpenSession())
+            using (var transaction = dbSession.BeginTransaction(System.Data.IsolationLevel.RepeatableRead))
+            {
+                var success = Submit(qSession, question, answer, dbSession, out isException);
+
+                transaction.Commit();
+
+                return success;
+            }
+        }
+
+        /// <summary>
+        /// Submit an answer using opened session and check whether the answer is exceptional answer on specified question.
+        /// </summary>
+        /// <param name="qSession">The session submitted from.</param>
+        /// <param name="question">The question of submitting answer.</param>
+        /// <param name="answer">The answer.</param>
+        /// <param name="dbSession">The opened session.</param>
+        /// <param name="isException">Indicate whether the specified answer is exceptional answer.</param>
+        /// <returns>True on success, False otherwise.</returns>
+        public bool Submit(Session qSession, Question question, Answer answer, ISession dbSession, out bool isException)
+        {
+            /// Check relation of <see cref="Session"/> , <see cref="Question"/> and <see cref="Answer"/>
             if (!qSession.QuestionnaireID.Equals(question.QuestionnaireID) ||
                 !question.ID.Equals(answer.QuestionID))
                 throw new InvalidOperationException($"Invalid submission!");
 
-            var cacheKey = question;
+            var answerID = dbSession.Save(answer);
 
-            try
-            {
-                var dbSession = this.sessionCache.Get(cacheKey);
+            isException = this.answerExceptionDataStore
+                .IsExceptionalAnswer(answer, dbSession);
 
-                var answerID = dbSession.Save(answer);
-                isException = this.answerExceptionDataStore
-                    .IsExceptionalAnswer(answer, dbSession);
-
-                return answerID != null;
-            }
-            finally
-            {
-                if (!more)
-                    this.sessionCache.Clear(cacheKey);
-            }
+            return answerID != null;
         }
     }
 }
